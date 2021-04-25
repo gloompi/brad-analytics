c6bdefe660e3179e44f9dd5c387f71e4e376b5fc
gloompi <gloompi@gmail.com>
2021-04-25 22:23:05 +0600

    pages

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -40,44 +40,109 @@ function createMessage(val, threshold) {
   return p;
 }
 
+function getDistributedData(data) {
+  const mobileData = data.filter(({ agent }) => agent === 'mobile');
+  const desktopData = data.filter(({ agent }) => agent !== 'mobile');
+  const mobile75P = p75(mobileData.sort((a, b) => a.val - b.val).map(({ val }) => val));
+  const desktop75P = p75(desktopData.sort((a, b) => a.val - b.val).map(({ val }) => val));
+
+  return { mobile75P, desktop75P };
+}
+
+function originSummary({ clsData, lcpData, fidData }) {
+  const clsContainer = document.querySelector('#cls');
+  const lcpContainer = document.querySelector('#lcp');
+  const fidContainer = document.querySelector('#fid');
+
+  const cls = getDistributedData(clsData);
+  const lcp = getDistributedData(lcpData);
+  const fid = getDistributedData(fidData);
+
+  // Origin summary
+  [
+    { container: clsContainer, data: cls, threshold: thresholds.CLS[0] },
+    { container: lcpContainer, data: lcp, threshold: thresholds.LCP[0] },
+    { container: fidContainer, data: fid, threshold: thresholds.FID[0] },
+  ].forEach(({ container, data, threshold }) => {
+    const desktopMessage = createMessage(data.desktop75P, threshold);
+    const mobileMessage = createMessage(data.mobile75P, threshold);
+    const desktopHeader = document.createElement('h3');
+    const mobileHeader = document.createElement('h3');
+    
+    container.appendChild(desktopHeader.appendChild(document.createTextNode('Desktop: ')))
+    container.appendChild(desktopMessage);
+    container.appendChild(mobileHeader.appendChild(document.createTextNode('Mobile: ')))
+    container.appendChild(mobileMessage);
+  });
+};
+
+function pagesSummary({ clsData, fidData, lcpData }) {
+  const container = document.querySelector('#pagesContainer');
+
+  const pages = [
+    { data: clsData, name: 'CLS' },
+    { data: fidData, name: 'FID' },
+    { data: lcpData, name: 'LCP' },
+  ].reduce((acc, { data, name }) => {
+    data.forEach((metric) => {
+      if (acc[metric.url]) {
+        if (acc[metric.url][name]) {
+          acc[metric.url][name].push(metric);
+        } else {
+          acc[metric.url][name] = [metric];
+        }
+      } else {
+        acc[metric.url] = {};
+        acc[metric.url][name] = [metric]
+      }
+    });
+
+    return acc;
+  }, {});
+
+  Object.entries(pages).forEach(([name, { CLS = [], LCP = [], FID = [] }]) => {
+    const liElem = document.createElement('li');
+    const h2 = document.createElement('h2');
+
+    const cls = getDistributedData(CLS);
+    const lcp = getDistributedData(LCP);
+    const fid = getDistributedData(FID);
+    
+    h2.appendChild(document.createTextNode(name));
+    liElem.appendChild(h2);
+    liElem.classList.add('metrics__item');
+
+    [
+      { name: 'CLS', data: cls, threshold: thresholds.CLS[0] },
+      { name: 'LCP', data: lcp, threshold: thresholds.LCP[0] },
+      { name: 'FID', data: fid, threshold: thresholds.FID[0] },
+    ].forEach(({ name, data, threshold }) => {
+      const metricTitle = document.createElement('strong');
+      const desktopMessage = createMessage(data.desktop75P, threshold);
+      const mobileMessage = createMessage(data.mobile75P, threshold);
+      const desktopHeader = document.createElement('h3');
+      const mobileHeader = document.createElement('h3');
+      const br = document.createElement('br');
+      
+      liElem.appendChild(metricTitle.appendChild(document.createTextNode(name)));
+      liElem.appendChild(br);
+      liElem.appendChild(desktopHeader.appendChild(document.createTextNode('Desktop: ')));
+      liElem.appendChild(desktopMessage);
+      liElem.appendChild(mobileHeader.appendChild(document.createTextNode('Mobile: ')));
+      liElem.appendChild(mobileMessage);
+    });
+
+    container.appendChild(liElem);
+  });
+};
+
 (function() {
   document.addEventListener('DOMContentLoaded', async () => {
-    const clsContainer = document.querySelector('#cls');
-    const lcpContainer = document.querySelector('#lcp');
-    const fidContainer = document.querySelector('#fid');
-
     const { data: clsData } = await fetchData(metrics.CLS);
     const { data: lcpData } = await fetchData(metrics.LCP);
     const { data: fidData } = await fetchData(metrics.FID);
 
-    const clsMobile = clsData.filter(({ agent }) => agent === 'mobile');
-    const clsDesktop = clsData.filter(({ agent }) => agent !== 'mobile');
-    const lcpMobile = lcpData.filter(({ agent }) => agent === 'mobile');
-    const lcpDesktop = lcpData.filter(({ agent }) => agent !== 'mobile');
-    const fidMobile = fidData.filter(({ agent }) => agent === 'mobile');
-    const fidDesktop = fidData.filter(({ agent }) => agent !== 'mobile');
-
-    const clsMobile75thPercentile = p75(clsMobile.sort((a, b) => a.val - b.val).map(({ val }) => val));
-    const clsDesktop75thPercentile = p75(clsDesktop.sort((a, b) => a.val - b.val).map(({ val }) => val));
-    const lcpMobile75thPercentile = p75(lcpMobile.sort((a, b) => a.val - b.val).map(({ val }) => val));
-    const lcpDesktop75thPercentile = p75(lcpDesktop.sort((a, b) => a.val - b.val).map(({ val }) => val));
-    const fidMobile75thPercentile = p75(fidMobile.sort((a, b) => a.val - b.val).map(({ val }) => val));
-    const fidDesktop75thPercentile = p75(fidDesktop.sort((a, b) => a.val - b.val).map(({ val }) => val));
-
-    [
-      { container: clsContainer, desktop: clsDesktop75thPercentile, mobile: clsMobile75thPercentile, threshold: thresholds.CLS[0] },
-      { container: lcpContainer, desktop: lcpDesktop75thPercentile, mobile: lcpMobile75thPercentile, threshold: thresholds.LCP[0] },
-      { container: fidContainer, desktop: fidDesktop75thPercentile, mobile: fidMobile75thPercentile, threshold: thresholds.FID[0] },
-    ].forEach(({ container, desktop, mobile, threshold }) => {
-      const desktopMessage = createMessage(desktop, threshold);
-      const mobileMessage = createMessage(mobile, threshold);
-      const desktopHeader = document.createElement('h3');
-      const mobileHeader = document.createElement('h3');
-      
-      container.appendChild(desktopHeader.appendChild(document.createTextNode('Desktop: ')))
-      container.appendChild(desktopMessage);
-      container.appendChild(mobileHeader.appendChild(document.createTextNode('Mobile: ')))
-      container.appendChild(mobileMessage);
-    });
+    originSummary({ clsData, lcpData, fidData });
+    pagesSummary({ clsData, lcpData, fidData });
   });
 }());
